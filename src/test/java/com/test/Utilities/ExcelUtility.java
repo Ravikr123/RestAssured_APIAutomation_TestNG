57c3477b8c7c802b5bd0a40bfbaeb2b7e59fd19a
Ravi Kumar <Ravi_Kumar62@Dell.com>
2020-05-17 22:22:42 +0530

    Added Method to return total Number of RowCount

--- src/test/java/com/test/Utilities/ExcelUtility.java
+++ src/test/java/com/test/Utilities/ExcelUtility.java
@@ -1,7 +1,9 @@
 package com.test.Utilities;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -17,7 +19,15 @@ public class ExcelUtility {
 	public static XSSFRow row;
 	public static XSSFCell cell;
 	
-	public static void getRowCount(String xlfile, String xlsheet) {
+	public static int getRowCount(String xlfile, String xlsheet) throws IOException {
+		
+		fi= new FileInputStream(xlfile);
+		wb = new XSSFWorkbook(fi);
+		ws = wb.getSheet(xlsheet);
+		int total_rowCount = ws.getLastRowNum();
+		wb.close();
+		fi.close();
+		return total_rowCount;
 		
 	}
 
